35f2239caa1596058c9d8843585f930e22b52dbf
galeblon <s171600@student.pg.edu.pl>
2021-03-09 12:58:12 +0100

    Building graphs from g6 sequence, basic edge methods

--- simple_graphs.c
+++ simple_graphs.c
@@ -52,7 +52,7 @@ static PyObject* AdjacencyMatrix_vertices(AdjacencyMatrixObject* self, PyObject 
 static PyObject* AdjacencyMatrix_vertex_degree(AdjacencyMatrixObject* self, PyObject* o)
 {
     if(PyLong_Check(o)) {
-        uint64_t e_temp = self->edges_matrix[PyLong_AsLong(o)%64];
+        uint64_t e_temp = self->edges_matrix[PyLong_AsLong(o)%MAX_VERTICES];
         uint degree = 0;
         for(size_t ei=0; ei<MAX_VERTICES; ei++) {
             degree += (e_temp)&0x01;
@@ -94,6 +94,46 @@ static PyObject* AdjacencyMatrix_add_vertex(AdjacencyMatrixObject* self, PyObjec
     }
     return Py_BuildValue("");
 };
+
+
+static PyObject* AdjacencyMatrix_delete_vertex(AdjacencyMatrixObject* self, PyObject* o)
+{
+    uint64_t v = PyLong_AsLong(o) % MAX_VERTICES;
+    if(PyLong_Check(o)) {
+        self->vertices &= ~(((uint64_t) 1) << v);
+        self->edges_matrix[v] = 0;
+        for(size_t vi=0; vi<v; vi++)
+            self->edges_matrix[vi] &= ~((uint64_t)1 << v);
+    } else {
+        PyErr_SetString(PyExc_TypeError, "Expected integer number as argument");
+        return NULL;
+    }
+    return Py_BuildValue("");
+};
+
+
+static PyObject* AdjacencyMatrix_number_of_edges(AdjacencyMatrixObject* self, PyObject *Py_UNUSED(ignored))
+{
+    uint64_t edges = 0;
+    for(size_t vi = 0; vi<MAX_VERTICES; vi++) {
+        uint64_t e_temp = self->edges_matrix[vi];
+        for(size_t ei=0; ei<MAX_VERTICES; ei++) {
+            edges += (e_temp)&0x01;
+            e_temp >>= 1;
+        }
+    }
+    return PyLong_FromLong(edges/2);
+}
+
+static PyObject* AdjacencyMatrix_edges(AdjacencyMatrixObject* self, PyObject *Py_UNUSED(ignored))
+{
+    PyObject* e_set = PySet_New(NULL);
+    for(uint64_t vi=0; vi<MAX_VERTICES; vi++)
+        for(uint64_t vj=vi+1; vj<MAX_VERTICES; vj++)
+            if(self->edges_matrix[vi] & ((uint64_t)1 << vj))
+                PySet_Add(e_set, PyTuple_Pack(2, PyLong_FromLong(vi), PyLong_FromLong(vj)));
+    return e_set;
+}
 // Graph methods end
 
 static PyMemberDef AdjacencyMatrix_members[] ={
@@ -107,10 +147,10 @@ static PyMethodDef AdjacencyMatrix_methods[] = {
     {"vertex_degree", (PyCFunction) AdjacencyMatrix_vertex_degree, METH_O, "Returns the degree of given vertex."},
     {"vertex_neighbors", (PyCFunction) AdjacencyMatrix_vertex_neighbors, METH_O, "Returns the neighbors of given vertex."},
     {"add_vertex", (PyCFunction) AdjacencyMatrix_add_vertex, METH_O, "Adds new vertex to the graph."},
+    {"delete_vertex", (PyCFunction) AdjacencyMatrix_delete_vertex, METH_O, "Removes existing vertex from the graph."},
+    {"number_of_edges", (PyCFunction) AdjacencyMatrix_number_of_edges, METH_NOARGS, "Returns the number of edges in graph."},
+    {"edges", (PyCFunction) AdjacencyMatrix_edges, METH_NOARGS, "Returns the edges of the graph."},
     //TODO WIP methods
-    {"delete_vertex", (PyCFunction) AdjacencyMatrix_method_test3, METH_O, "Removes existing vertex from the graph."},
-    {"number_of_edges", (PyCFunction) AdjacencyMatrix_method_test2, METH_NOARGS, "Returns the number of edges in graph."},
-    {"edges", (PyCFunction) AdjacencyMatrix_method_test2, METH_NOARGS, "Returns the edges of the graph."},
     {"is_edge", (PyCFunction) AdjacencyMatrix_method_test2, METH_VARARGS, "Returns whether the vertices form an edge in the graph."},
     {"add_edge", (PyCFunction) AdjacencyMatrix_method_test2, METH_VARARGS, "Adds new edge between specified vertices in the graph."},
     {"delete_edge", (PyCFunction) AdjacencyMatrix_method_test2, METH_VARARGS, "Removed edge between specified vertices in the graph."},
@@ -144,15 +184,31 @@ static int AdjacencyMatrix_init(AdjacencyMatrixObject *self, PyObject *args, PyO
         return -1;
     }
 
+    //Vertices
     char* g6_sequence = str;
     uint64_t num_of_vertices = g6_sequence[0] - 63;
-    uint64_t index = 1;
-    while(num_of_vertices > 0) {
-        num_of_vertices--;
-        self->vertices |= index;
-        index<<=1;
+    while(num_of_vertices > 0)
+        self->vertices |=  (uint64_t)1 << --num_of_vertices;
+    num_of_vertices = g6_sequence[0] - 63;
+    //Edges
+    size_t g6_index = 1;
+    char n_vect = g6_sequence[g6_index] - 63;
+    char offset = 5;
+    char counter = 6;
+    for(size_t vi = 0; vi < num_of_vertices; vi++) {
+        for(size_t vj = 0; vj < vi; vj++) {
+            if(n_vect & (1 << offset)) {
+                self->edges_matrix[vi] |= (uint64_t)1 << vj;
+                self->edges_matrix[vj] |= (uint64_t)1 << vi;
+            }
+            n_vect <<= 1;
+            if(--counter == 0) {
+                counter = 6;
+                n_vect = g6_sequence[++g6_index] - 63;
+            }
+        }
     }
-    //TODO add edges
+    // TODO also fill lower triangle
     return 0;
 }
 
