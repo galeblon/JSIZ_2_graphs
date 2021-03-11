63e6a88c88f68f7a0db97b9f83ed2795bfa67ae5
galeblon <s171600@student.pg.edu.pl>
2021-03-11 13:48:22 +0100

    Script fix

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 python3.9 setup.py build
-cp build/lib.linux-x86_64-3.8/simple_graphs.cpython-38-x86_64-linux-gnu.so simple_graphs.so
+cp build/lib.linux-x86_64-3.9/simple_graphs.cpython-39-x86_64-linux-gnu.so simple_graphs.so
